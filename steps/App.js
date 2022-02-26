3d902db3905455a995a462378b0ed97821c58f00
camillefenson <camillefenson@icloud.com>
2022-02-26 14:48:49 -0800

    commin on main

--- steps/App.js
+++ steps/App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, React, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>progress bar will go here</Text>
+      <View style={styles.buttonplacement}>
+        <View style={styles.buttoncontainer}>
+          <TouchableOpacity onPress={() =>
+            console.log("Button 1")
+          }>
+            <Text style={styles.buttons}>Button 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>
+            console.log("Button 2")
+          }>
+            <Text style={styles.buttons}>Button 2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() =>
+            console.log("Button 3")
+          }>
+            <Text style={styles.buttons}>Button 3</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -15,6 +34,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 45,
   },
+  buttoncontainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  buttonplacement: {
+    backgroundColor: '#fff',
+    justifyContent: 'flex-end',
+    height: 625,
+    padding: 20,
+  },
+  buttons: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 20,
+    paddingLeft: 37,
+    paddingRight: 37,
+  },
+  foodPic: {
+    justifyContent: 'center',
+    
+  }
 });
